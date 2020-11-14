43bdf50d6d90ba0fc197962353d6f687e14ad1fd
Ove-013 <lizhe9613@live.com>
2020-11-14 16:01:45 +0800

    菜单按钮动态激活（根据路由跳转）

--- src/components/serve/Menu.vue
+++ src/components/serve/Menu.vue
@@ -2,18 +2,17 @@
   <div class="menu">
     <!-- 菜单栏 -->
     <el-menu
-      default-active="1"
+      :default-active="$route.path"
       class="el-menu-vertical-demo"
-      @open="handleOpen"
-      @close="handleClose"
+      :collapse="isCollapse"
+      router
+      scroll
     >
       <!-- 首页统计 默认 -->
-      <router-link class="text-decoration-none" to="/serve">
-        <el-menu-item index="1">
-          <i class="el-icon-pie-chart"></i>
-          <span slot="title">首页统计</span>
-        </el-menu-item>
-      </router-link>
+      <el-menu-item index="/serve">
+        <i class="el-icon-pie-chart"></i>
+        <span slot="title">首页统计</span>
+      </el-menu-item>
       <!-- 用户信息 （医生、患者等） -->
       <el-submenu index="2">
         <div slot="title" class="title">
@@ -22,29 +21,21 @@
         </div>
         <el-menu-item-group>
           <!-- 医生信息 -->
-          <router-link class="text-decoration-none" to="/serve/users/doc">
-            <el-menu-item index="2-1">医生</el-menu-item>
-          </router-link>
+          <el-menu-item index="/serve/users/doc">医生</el-menu-item>
           <!-- 患者信息 -->
-          <router-link class="text-decoration-none" to="/serve/users/patients">
-            <el-menu-item index="2-2">患者信息</el-menu-item>
-          </router-link>
+          <el-menu-item index="/serve/users/patients">患者信息</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
       <!-- 挂号信息 （当前挂号，已就诊等） -->
-      <router-link class="text-decoration-none" to="/serve/registration">
-        <el-menu-item index="3">
-          <i class="el-icon-first-aid-kit"></i>
-          <span slot="title">挂号信息</span>
-        </el-menu-item>
-      </router-link>
+      <el-menu-item index="/serve/registration">
+        <i class="el-icon-first-aid-kit"></i>
+        <span slot="title">挂号信息</span>
+      </el-menu-item>
       <!-- 排班信息 -->
-      <router-link class="text-decoration-none" to="/serve/working">
-        <el-menu-item index="4">
-          <i class="el-icon-s-order"></i>
-          <span slot="title">排班信息</span>
-        </el-menu-item>
-      </router-link>
+      <el-menu-item index="/serve/working">
+        <i class="el-icon-s-order"></i>
+        <span slot="title">排班信息</span>
+      </el-menu-item>
       <!-- 药品管理 -->
       <el-submenu index="5">
         <div slot="title" class="title">
@@ -53,22 +44,16 @@
         </div>
         <el-menu-item-group>
           <!-- 收益信息 -->
-          <router-link class="text-decoration-none" to="/serve/logistics/charge">
-            <el-menu-item index="5-1">收费信息</el-menu-item>
-          </router-link>
+          <el-menu-item index="/serve/logistics/charge">收费信息</el-menu-item>
           <!-- 药房管理 -->
-          <router-link class="text-decoration-none" to="/serve/logistics/drug">
-            <el-menu-item index="5-2">药房管理</el-menu-item>
-          </router-link>
+          <el-menu-item index="/serve/logistics/drug">药房管理</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
       <!-- 系统设置 -->
-      <router-link class="text-decoration-none" to="/serve/system">
-        <el-menu-item index="6">
-          <i class="el-icon-setting"></i>
-          <span slot="title">系统管理</span>
-        </el-menu-item>
-      </router-link>
+      <el-menu-item index="/serve/system">
+        <i class="el-icon-setting"></i>
+        <span slot="title">系统管理</span>
+      </el-menu-item>
     </el-menu>
   </div>
 </template>
