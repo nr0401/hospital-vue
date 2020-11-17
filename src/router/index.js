175c0b3202c4a9999a3b4368310deac5eb509618
Ove-013 <lizhe9613@live.com>
2020-11-17 17:39:19 +0800

    about无用组件

--- src/router/index.js
+++ src/router/index.js
@@ -109,15 +109,6 @@ const routes = [
       component: client
     },
     {
-      path: 'about',
-      name: 'About',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import( /* webpackChunkName: "about" */ '../views/About.vue')
-    },
-    {
       path: 'introducedpage',
       name: 'Introducedpage',
       component: Introducedpage
